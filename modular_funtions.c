74eb81066810ee7bf7724a05751264a376327a37
giaedgington <gia.edgington@gmail.com>
2021-06-23 21:54:04 -0400

    Assign unit value

--- modular_funtions.c
+++ modular_funtions.c
@@ -5,8 +5,14 @@ int main(void){
     scanf("%d", &num);
     int vals[num];
     int i;
+    char unit;
     for(i = 0; i < num; i++){
         scanf("%d", &vals[i]);
+        int j=0;
+        while(vals[i][j] != '\0'){
+            j++;
+        }
+        unit = vals[i][j+1];
     }
     
     return 0;
